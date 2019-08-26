f84d27d04a7052e07f67be3c6b55f2f84191cf80
Practicalgnome <practicalgnome@gmail.com>
2019-08-26 15:20:26 +0300

    s286

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -3,6 +3,7 @@ import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +11,14 @@ import { Subject } from 'rxjs';
 export class RecipesService {
   recipesUpdated = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A test Rec',
-      'Test description',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Salad', 3)
-      ]),
-    new Recipe(
-      'Second',
-      'second descr',
-      'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg',
-      [
-        new Ingredient('Buns', 7),
-        new Ingredient('Cheese', 4)
-      ])
-  ];
+  private recipes: Recipe[];
 
   constructor(private listService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesUpdated.next([...this.recipes]);
+  }
 
   addRecipe(recipe) {
     this.recipes.push(recipe);
